ab763b41930f1401c2427cde2b830b8ed827ecfc
pajmera4 <pajmera@thoughtworks.com>
2022-09-21 00:44:43 +0530

    BAH-2209 Update. Fix test coverage issue

--- src/utils/api-utils.ts
+++ src/utils/api-utils.ts
@@ -1,13 +1,3 @@
-export const postApiCall = (url, data) => {
-  return fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  })
-}
-
 export const getApiCall = async url => {
   const response = await fetch(url, {
     method: 'GET',
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,5 @@
 export const streamingURL =
   process.env.SPEECH_ASSISTANT_PROXY_URL || location.origin
 export const language = 'en'
-export const saveNotesUrl = '/openmrs/ws/rest/v1/obs'
-export const conceptUrl = '/openmrs/ws/rest/v1/concept?q="Consultation Note'
 export const visitUrl = (patientId, locationId) =>
   `/openmrs/ws/rest/v1/visit?includeInactive=false&patient=${patientId}&location=${locationId}&v=custom:(uuid,visitType,startDatetime,stopDatetime,encounters)`
